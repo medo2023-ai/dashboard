4999b630086139e50eca4ba4c1842ae6614d511e
Salsabeel Ebrahim <salsabeelibrahem03@gmail.com>
2025-08-26 21:25:18 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { Button } from './components/ui/button';
+
+function App() {
+
+  return (
+    <>
+    
+      <p className="read-the-docs bg-red-300">
+        Click on the Vite and React logos to learn more
+      </p>
+<Button variant="outline">Button</Button>
+    </>
+  )
+}
+
+export default App
